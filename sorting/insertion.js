0a63160662bba830b17df2eb585c98d8306f5d67
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-28 18:02:58 -0400

    update files

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,26 +1,26 @@
-const drink = {
-    drink1: {
+const drink = [
+    {
         name: "ABC",
         price: 2
     },
 
-    dirnk2: {
+    {
         name: "Black Cat",
         price: 120
     },
-    dirnk3: {
+    {
         name: "Krud",
         price: 499 
     },
-    dirnk4: {
+    {
         name: "Tiger",
         price: 300 
     },
-    dirnk5: {
+    {
         name: "Cambodia",
         price: 99 
     }
-}
+]
 
-console.log(drink);
+console.log(drink[1]);
 
